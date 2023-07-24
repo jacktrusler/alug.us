64c1026ee1725b31974d54250312cf5bb5af1185
jacktrusler <jtrus93@gmail.com>
2023-07-24 08:55:04 -0400

    starting fall presentation, cleaning extra code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const logo = document.getElementById("logo");
+const toggle = document.getElementById("theme-toggle");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
 
 const storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 if (storedTheme) {
@@ -6,19 +8,46 @@ if (storedTheme) {
 }
 
 const cT = document.documentElement.getAttribute("data-theme");
+if (cT === "dark") { moon.setAttribute("class", "moon hidden"); }
+if (cT === "light") { sun.setAttribute("class", "sun hidden"); }
 
-logo.onclick = function() {
+toggle.onclick = function() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   let targetTheme = "light";
 
   if (currentTheme === "light") {
     targetTheme = "dark";
+    moon.setAttribute("class", "moon hidden")
+    sun.setAttribute("class", "sun");
+  } else {
+    sun.setAttribute("class", "sun hidden");
+    moon.setAttribute("class", "moon");
   }
 
   document.documentElement.setAttribute('data-theme', targetTheme);
   localStorage.setItem('theme', targetTheme);
 };
 
-if (storedTheme) {
-  document.documentElement.setAttribute('data-theme', storedTheme);
+/**
+ * Downloads by Url, necessary to include name and filetype as second and third arguments.
+ * @param {string} url - The url to download from.
+ * @param {string} name - The name the download will be saved as.
+*/
+async function downloadByUrl(url, name) {
+  try {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+    link.download = name;
+    link.click();
+    link.remove();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+function downloadVimrc() {
+  downloadByUrl("https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc", "vimrc_example");
 }
--- pages/internet_fall/fall.js
+++ pages/internet_fall/fall.js
@@ -2,21 +2,21 @@ const toggle = document.getElementById("theme-toggle");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
 
-const storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "business" : "cupcake";
+const storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 if (storedTheme) {
   document.documentElement.setAttribute('data-theme', storedTheme);
 }
 
 const cT = document.documentElement.getAttribute("data-theme");
-if (cT === "business") { moon.setAttribute("class", "moon hidden"); }
-if (cT === "cupcake") { sun.setAttribute("class", "sun hidden"); }
+if (cT === "dark") { moon.setAttribute("class", "moon hidden"); }
+if (cT === "light") { sun.setAttribute("class", "sun hidden"); }
 
 toggle.onclick = function() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
-  let targetTheme = "cupcake";
+  let targetTheme = "light";
 
-  if (currentTheme === "cupcake") {
-    targetTheme = "business";
+  if (currentTheme === "light") {
+    targetTheme = "dark";
     moon.setAttribute("class", "moon hidden")
     sun.setAttribute("class", "sun");
   } else {
@@ -27,7 +27,3 @@ toggle.onclick = function() {
   document.documentElement.setAttribute('data-theme', targetTheme);
   localStorage.setItem('theme', targetTheme);
 };
-
-function downloadVimrc() {
-  downloadByUrl("https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc", "vimrc_example");
-}

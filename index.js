abd521ff9d0796a02e80776eb44bc49ec5c99174
jacktrusler <jtrus93@gmail.com>
2023-07-26 23:16:04 -0400

    cooking on fall of the internet

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+/**
+ * Downloads by Url, necessary to include name and filetype as second and third arguments.
+ * @param {string} url - The url to download from.
+ * @param {string} name - The name the download will be saved as.
+*/
+async function downloadByUrl(url, name) {
+  try {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+    link.download = name;
+    link.click();
+    link.remove();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+function downloadVimrc() {
+  downloadByUrl("https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc", "vimrc_example");
+}
+
+// ----------------------------
+// Toggling Dark and Light mode
+// ----------------------------
 const toggle = document.getElementById("theme-toggle");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
@@ -28,26 +55,3 @@ toggle.onclick = function() {
   localStorage.setItem('theme', targetTheme);
 };
 
-/**
- * Downloads by Url, necessary to include name and filetype as second and third arguments.
- * @param {string} url - The url to download from.
- * @param {string} name - The name the download will be saved as.
-*/
-async function downloadByUrl(url, name) {
-  try {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const link = document.createElement("a");
-
-    link.href = URL.createObjectURL(blob);
-    link.download = name;
-    link.click();
-    link.remove();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-function downloadVimrc() {
-  downloadByUrl("https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc", "vimrc_example");
-}

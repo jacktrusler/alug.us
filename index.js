32edbd4fb6c05b82956aab4e363ba1dd1f2cb70d
jacktrusler <jtrus93@gmail.com>
2025-02-22 16:50:17 -0500

    adding club details to the page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Downloads by Url, necessary to include name and filetype as second and third arguments.
  * @param {string} url - The url to download from.
  * @param {string} name - The name the download will be saved as.
-*/
+ */
 async function downloadByUrl(url, name) {
   try {
     const response = await fetch(url);
@@ -16,10 +16,13 @@ async function downloadByUrl(url, name) {
   } catch (err) {
     console.log(err);
   }
-};
+}
 
 function downloadVimrc() {
-  downloadByUrl("https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc", "vimrc_example");
+  downloadByUrl(
+    "https://raw.githubusercontent.com/jacktrusler/dotfiles/main/.vimrc",
+    "vimrc_example",
+  );
 }
 
 /* ----------------------------
@@ -30,83 +33,99 @@ const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
 const logo = document.getElementById("logo-div");
 
-const storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+const storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
 if (storedTheme) {
-  document.documentElement.setAttribute('data-theme', storedTheme);
+  document.documentElement.setAttribute("data-theme", storedTheme);
 }
 
 const cT = document.documentElement.getAttribute("data-theme");
-if (cT === "dark" && moon) { moon.setAttribute("class", "moon hidden"); }
-if (cT === "light" && sun) { sun.setAttribute("class", "sun hidden"); }
+if (cT === "dark" && moon) {
+  moon.setAttribute("class", "moon hidden");
+}
+if (cT === "light" && sun) {
+  sun.setAttribute("class", "sun hidden");
+}
 
 if (toggle) {
-  toggle.onclick = function() {
+  toggle.onclick = function () {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     let targetTheme = "light";
 
     if (currentTheme === "light") {
       targetTheme = "dark";
-      moon.setAttribute("class", "moon hidden")
+      moon.setAttribute("class", "moon hidden");
       sun.setAttribute("class", "sun");
     } else {
       sun.setAttribute("class", "sun hidden");
       moon.setAttribute("class", "moon");
     }
 
-    document.documentElement.setAttribute('data-theme', targetTheme);
-    localStorage.setItem('theme', targetTheme);
-  }
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem("theme", targetTheme);
+  };
 }
 
 if (logo) {
-  logo.onclick = function() {
+  logo.onclick = function () {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     let targetTheme = "light";
     if (currentTheme === "light") {
       targetTheme = "dark";
     }
-    document.documentElement.setAttribute('data-theme', targetTheme);
-    localStorage.setItem('theme', targetTheme);
-  }
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem("theme", targetTheme);
+  };
 }
+
+// ----------------------------
+// Other JS
+// ----------------------------
+
+const detailsBtn = document.getElementById("details-btn");
+const detailsDialog = document.getElementById("details-dialog");
+const close = document.getElementById("close");
+
+detailsBtn.addEventListener("click", function () {
+  detailsDialog.showModal();
+});
+
+close.addEventListener("click", function () {
+  detailsDialog.close();
+});
 
 // ----------------------------
 // JQuery
 // ----------------------------
 
-const years = [
-  '2021',
-  '2022',
-  '2023',
-]
+const years = ["2021", "2022", "2023"];
 
 const months = [
-  'january',
-  'february',
-  'march',
-  'april',
-  'may',
-  'june',
-  'july',
-  'august',
-  'september',
-  'october',
-  'november',
-  'december',
-]
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
 
 for (const year of years) {
   $(`#${year}-container`).hide();
-  $(`#${year}-year`).click(function() {
+  $(`#${year}-year`).click(function () {
     $(`#${year}-container`).toggle(500);
-  })
+  });
 
   for (const month of months) {
     $(`#${month}-${year}-container`).hide();
-    $(`#${month}-${year}`).click(function() {
+    $(`#${month}-${year}`).click(function () {
       $(`#${month}-${year}-container`).toggle(300);
-    })
+    });
   }
-
 }
-
